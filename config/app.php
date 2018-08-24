e27fb13ed3718b32f8832f4498e5a590b3f14575
John Doe <mario.florence215@gmail.com>
2018-08-24 09:47:46 -0700

    changes

--- config/app.php
+++ config/app.php
@@ -103,10 +103,10 @@ return [
     | will not be safe. Please do this before deploying an application!
     |
     */
-
-    'key' => env('APP_KEY'),
+    'key' => env('APP_KEY', 'SomeRandomStringSomeRandomString'),
 
     'cipher' => 'AES-256-CBC',
+  
 
     /*
     |--------------------------------------------------------------------------
